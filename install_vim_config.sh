30b1fa553b60f641cb1628dfee14bcabb9ec6fef
eric <eric@voxelfarm.com>
2024-04-09 16:35:29 -0400

    Added Install Scripts

---
+++ install_vim_config.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# copy .vimrc to ~/.vimrc
+cp .vimrc ~/.vimrc
+
+# make directory ~/.vim
+mkdir -p ~/.vim
+
+# copy .vim to ~.vim
+cp -rp .vim/* ~/.vim
+
+# note sure why but the git folder is not copied properly..
+# until then keep uncomment code below
+
+# current directory ~/.vim/bundle/Vundle.vim
+cd ~/.vim/bundle
+
+# remove Vundle.vim
+rm -rf Vundle.vim
+
+# make directory Vundle.vim
+mkdir -p Vundle.vim
+
+# git clone Vundle
+git clone https://github.com/VundleVim/Vundle.vim Vundle.vim

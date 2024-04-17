3fea2603972ad37cf2b09c8876068ad6072a064f
eric <eric@voxelfarm.com>
2024-04-17 15:10:35 -0400

    Fixed Telescope FZF-Native Build on Windows

--- .config/nvim/lua/ericnantel/plugins/telescope.lua
+++ .config/nvim/lua/ericnantel/plugins/telescope.lua
@@ -3,7 +3,13 @@ return {
     branch = "0.1.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            -- build = "make",
+            -- NOTE:
+            -- It builds for Windows; however having issues when opening telescope in some lua files..
+            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        },
         "nvim-tree/nvim-web-devicons",
     },
     config = function()

a50301a07e9877879d348a44acfd2dbb7ffd43b9
Eric Nantel <eric@voxelfarm.com>
2024-04-19 10:34:32 -0400

    Updated NeoVim Plugins

--- .config/nvim/lua/ericnantel/lazy.lua
+++ .config/nvim/lua/ericnantel/lazy.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     { import = "ericnantel.plugins" },
     { import = "ericnantel.plugins.lsp" },
+    { import = "ericnantel.plugins.colorscheme" },
 }, {
     checker = {
         enabled = true,

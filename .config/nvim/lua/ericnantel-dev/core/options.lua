2c6267b02e9d37762e984cf267ff5cef1932dc7b
Eric Nantel <eric@voxelfarm.com>
2024-05-30 08:44:04 -0400

    Misc Nvim dev options

---
+++ .config/nvim/lua/ericnantel-dev/core/options.lua
@@ -0,0 +1,20 @@
+
+-- vim.opt.inccommand = "split"
+-- vim.opt.formatoptions:remove "o"
+-- vim.opt.shada = { "'10", "<0", "s10", "h" }
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true

89bb81c1fbbd9564fdbaf0dfe816defafbf469a5
Eric Nantel <eric@voxelfarm.com>
2024-06-05 11:58:05 -0400

    Added Nvim dev Plugin

---
+++ .config/nvim/lua/ericnantel-dev/ccc.lua
@@ -0,0 +1,8 @@
+local ccc = require 'ccc'
+
+ccc.setup {
+	highlighter = {
+		auto_enable = true,
+		lsp = true,
+	}
+}
---
+++ .config/nvim/lua/ericnantel-dev/plugins/ccc.lua
@@ -0,0 +1,6 @@
+return {
+	"uga-rosa/ccc.nvim",
+	config = function()
+		require 'ericnantel-dev.ccc'
+	end,
+}

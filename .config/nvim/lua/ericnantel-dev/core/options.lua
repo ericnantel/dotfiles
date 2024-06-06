25991bfc0056f24751ef1b50b0eaa2760812327b
Eric Nantel <eric@voxelfarm.com>
2024-06-06 09:07:14 -0400

    Added Commentary Support For Z80 Filetype

--- .config/nvim/lua/ericnantel-dev/core/options.lua
+++ .config/nvim/lua/ericnantel-dev/core/options.lua
@@ -12,6 +12,7 @@ vim.opt.background = "dark"
 vim.cmd [[ colorscheme darcula ]]
 
 vim.cmd [[autocmd BufNewFile,BufRead *.z80.asm set filetype=z80]]
+vim.cmd [[autocmd FileType z80 setlocal commentstring=;\ %s]]
 vim.cmd [[autocmd FileType alpha setlocal nofoldenable]]
 
 vim.opt.title = false

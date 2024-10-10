043a79d36af905bf059056102a4d791bfd16c7b7
Eric Nantel <eric@voxelfarm.com>
2024-10-10 12:53:34 -0400

    Updated Wezterm Config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,29 +1,32 @@
 local wezterm = require("wezterm")
 
-local config = {}
+local config = wezterm.config_builder()
 
---- Set Pwsh as the default on Windows
--- config.default_prog = {
--- 	'pwsh.exe', '-NoLogo'
--- }
-
-config.window_background_opacity = 1.0
-
---config.color_scheme = "catppuccin"
---config.color_scheme = "Brogrammer"
-config.color_scheme = "Pro"
-
---config.color_scheme = "JetBrains Darcula"
---config.color_scheme = "Zenburn"
-
-config.enable_scroll_bar = false
-config.enable_tab_bar = false
-config.use_fancy_tab_bar = false
-
---config.font = wezterm.font("JetBrainsMonoNL Nerd Font Propo", { weight = "Bold", stretch = "Normal", italic = false })
-config.font_size = 16
-
-config.send_composed_key_when_left_alt_is_pressed = true
-config.send_composed_key_when_right_alt_is_pressed = true
+config = {
+	automatically_reload_config = true,
+	enable_scroll_bar = false,
+	enable_tab_bar = false,
+	use_fancy_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	-- window_decorations = "RESIZE",
+	window_background_opacity = 1.0,
+	window_padding = { left = 3, right = 3, top = 0, bottom = 0 },
+	-- background = {
+	-- 	{
+	-- 		source = {
+	-- 			File = "",
+	-- 		},
+	-- 		width = "100%",
+	-- 		height = "100%",
+	-- 		opacity = 0.55,
+	-- 	},
+	-- },
+	default_cursor_style = "SteadyBlock",
+	font = wezterm.font("JetBrainsMonoNL Nerd Font Propo", { weight = "Bold", stretch = "Normal", italic = false }),
+	font_size = 16,
+	color_scheme = "Pro",
+	send_composed_key_when_left_alt_is_pressed = true,
+	send_composed_key_when_right_alt_is_pressed = true,
+}
 
 return config

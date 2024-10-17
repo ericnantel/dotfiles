628d4c2e2b32818f681b1f4498fe6d3fc1b7fcac
eric <eric@voxelfarm.com>
2024-10-17 15:56:54 -0400

    Fixed Typo with Eza Alias in PowerShell

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,9 @@
 # Assumes starship is installed
 Invoke-Expression (&starship init powershell)
 
-#Assumes terminal icons module is installed
+# Assumes terminal icons module is installed
 # Import-Module -Name Terminal-Icons
 
-#Assumes eza is installed
-Set-Alias -Name ls -Value Eza_Icons
+# Assumes eza and a nerd-font (with icons) are installed
+Set-Alias -Name ls -Value Eza_Icons -Option AllScope
 function Eza_Icons { eza --icons $args }

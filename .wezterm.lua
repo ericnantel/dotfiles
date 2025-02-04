bb029cc1f1e65c8aed143f4173ccb7f78b9d8a79
eric <eric@voxelfarm.com>
2025-02-04 13:00:01 -0500

    Updated Wezterm Config

--- .wezterm.lua
+++ .wezterm.lua
@@ -24,6 +24,96 @@ elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
 	font_size = 14
 end
 
+local leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+local keys = {}
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	-- NOTE: On Windows, Tmux is not available
+	keys = {
+		-- vertical split
+		{
+			key = "v",
+			mods = "LEADER",
+			action = wezterm.action.SplitHorizontal({
+				domain = "CurrentPaneDomain",
+			}),
+		},
+		-- horizontal split
+		{
+			key = "h",
+			mods = "LEADER",
+			action = wezterm.action.SplitVertical({
+				domain = "CurrentPaneDomain",
+			}),
+		},
+		-- close current pane
+		{
+			key = "x",
+			mods = "LEADER",
+			action = wezterm.action.CloseCurrentPane({
+				confirm = true,
+			}),
+		},
+		-- maximize toggle current pane
+		{
+			key = "m",
+			mods = "LEADER",
+			action = wezterm.action.TogglePaneZoomState,
+		},
+		-- move to left pane
+		{
+			key = "h",
+			mods = "CTRL",
+			action = wezterm.action.ActivatePaneDirection("Left"),
+		},
+		-- move to right pane
+		{
+			key = "l",
+			mods = "CTRL",
+			action = wezterm.action.ActivatePaneDirection("Right"),
+		},
+		-- move to up pane
+		{
+			key = "k",
+			mods = "CTRL",
+			action = wezterm.action.ActivatePaneDirection("Up"),
+		},
+		-- move to down pane
+		{
+			key = "j",
+			mods = "CTRL",
+			action = wezterm.action.ActivatePaneDirection("Down"),
+		},
+		-- resize left current pane
+		{
+			key = "LeftArrow",
+			mods = "LEADER",
+			action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+		},
+		-- resize right current pane
+		{
+			key = "RightArrow",
+			mods = "LEADER",
+			action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+		},
+		-- resize up current pane
+		{
+			key = "UpArrow",
+			mods = "LEADER",
+			action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+		},
+		-- resize down current pane
+		{
+			key = "DownArrow",
+			mods = "LEADER",
+			action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+		},
+	}
+else
+	-- NOTE: It is possible to know if we are using tmux
+	-- See WEZTERM_IN_TMUX in documentation
+	-- However, I would recommend simply using tmux keybindings with tmux on MacOS or GNU Linux
+end
+
 config = {
 	default_prog = default_prog,
 	automatically_reload_config = true,
@@ -54,6 +144,8 @@ config = {
 	color_scheme = "Night Owl (Gogh)",
 	send_composed_key_when_left_alt_is_pressed = true,
 	send_composed_key_when_right_alt_is_pressed = true,
+	leader = leader,
+	keys = keys,
 }
 
 return config

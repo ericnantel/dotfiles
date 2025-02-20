d0b86cab572ccadb09f9b1436ef76deb97fb5a85
Eric Nantel <eric@voxelfarm.com>
2025-02-20 16:43:14 -0500

    Fixed Typo

--- install_keyboard_layouts.sh
+++ install_keyboard_layouts.sh
@@ -11,5 +11,5 @@ if [ "os" = 'Msys' ]; then
 	# TODO: Default path for RK Profiles ?
 elif [ "os" = 'Darwin' ] then
 	# NOTE: Requires a restart. See screenshots to see how I set Ctrl, Alt
-	cp keyboard-layouts/Canadien_FR.keylayout "~/Library/Keyboard Layouts/Canadien_FR.keylayout"
+	cp keyboard-layouts/MacOS/Canadien_FR.keylayout "~/Library/Keyboard Layouts/Canadien_FR.keylayout"
 fi

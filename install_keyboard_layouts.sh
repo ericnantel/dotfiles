3d5502deec863be72295942bdd4b79fbd90e365e
Eric Nantel <eric@voxelfarm.com>
2025-02-20 15:03:52 -0500

    Added Canadien_FR Keyboard Layout MacOS

---
+++ install_keyboard_layouts.sh
@@ -0,0 +1,15 @@
+
+#!/bin/sh
+
+os=$(uname -o)
+
+# init and update submodules
+git submodule init
+git submodule update
+
+if [ "os" = 'Msys' ]; then
+	# TODO: Default path for RK Profiles ?
+elif [ "os" = 'Darwin' ] then
+	# NOTE: Requires a restart. See screenshots to see how I set Ctrl, Alt
+	cp keyboard-layouts/Canadien_FR.keylayout "~/Library/Keyboard Layouts/Canadien_FR.keylayout"
+fi

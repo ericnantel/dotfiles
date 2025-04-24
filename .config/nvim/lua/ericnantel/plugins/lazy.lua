c9aabd9c7c1150023d7610af7a9cb7bf43c9da4a
eric <eric@voxelfarm.com>
2025-04-24 16:59:45 -0400

    Updated Neovim Plugin Config (Treesitter)

--- .config/nvim/lua/ericnantel/plugins/lazy.lua
+++ .config/nvim/lua/ericnantel/plugins/lazy.lua
@@ -484,11 +484,25 @@ lazy.setup({
 					},
 					highlight = {
 						enable = true,
-						disable = {
-							"glsl",
-							"markdown",
-							"markdown_inline",
-						},
+						-- disable = {
+						-- 	"glsl",
+						-- 	"markdown",
+						-- 	"markdown_inline",
+						-- },
+						disable = function(lang, buf)
+							if lang == "glsl" then
+								return true
+							elseif lang == "markdown" then
+								return true
+							elseif lang == "markdown_inline" then
+								return true
+							end
+							local max_filesize = 100 * 1024 -- 100Kb
+							local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+							if ok and stats and stats.size > max_filesize then
+								return true
+							end
+						end,
 						additional_vim_regex_highlighting = false,
 					},
 				})

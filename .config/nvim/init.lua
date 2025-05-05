fe89f978b2ae1f98cf18eb0e86869c0d64037e28
eric <eric@voxelfarm.com>
2025-05-05 17:02:18 -0400

    Refactored Neovim Config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 require("ericnantel.keymaps")
 require("ericnantel.options")
-require("ericnantel.plugins.lazy")
 require("ericnantel.plugins.keymaps")
-require("ericnantel.plugins.options")
+require("ericnantel.plugins.globals")
+require("ericnantel.plugins.lazy")
--- .config/nvim/lua/ericnantel/plugins/globals.lua
+++ .config/nvim/lua/ericnantel/plugins/globals.lua
@@ -1,23 +1,23 @@
--- vim-tmux-navigator options
+-- vim-tmux-navigator globals
 vim.g.tmux_navigator_no_mappings = 1
 vim.g.tmux_navigator_save_on_switch = 0
 -- vim.g.tmux_navigator_disable_when_zoomed = 1
 -- vim.g.tmux_navigator_preserve_zoom = 1
 vim.g.tmux_navigator_no_wrap = 1
 
--- vim-maximizer options
+-- vim-maximizer globals
 vim.g.maximizer_set_default_mapping = 1
 
--- vim-signature options
+-- vim-signature globals
 vim.g.SignatureWrapJumps = 1
 vim.g.SignatureMarkTextHLDynamic = 1
 
--- vim-commentary options
+-- vim-commentary globals
 vim.cmd([[autocmd FileType z80 setlocal commentstring=;\ %s]])
 vim.cmd([[autocmd FileType glsl setlocal commentstring=//\ %s]])
 vim.cmd([[autocmd FileType lisp setlocal commentstring=;;\ %s]])
 
--- vim-gitgutter options
+-- vim-gitgutter globals
 vim.g.gitgutter_enabled = 1
 vim.g.gitgutter_signs = 1
 vim.g.gitgutter_async = 1

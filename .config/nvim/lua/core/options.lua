3f6fec6e3c2e5fbe11d24c95a7a1ab99161055f1
Eric Nantel <eric@voxelfarm.com>
2025-05-06 19:47:53 -0400

    Updated Neovim Config (core/options.lua)

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,86 +1,25 @@
--- vim.opt.shada = { "'10", "<0", "s10", "h" }
-
--- vim.cmd [[ let g:netrw_liststyle=3 ]]
+-- disable netrw at the very start of your init.lua
+-- NOTE: see nvim-tree (plugins.lazy.lua)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.opt.syntax = on
-
-vim.opt.background = "dark"
--- vim.opt.termguicolors = true
-vim.cmd([[ colorscheme darcula ]])
-
-vim.cmd([[autocmd BufNewFile,BufRead *.z80.asm set filetype=z80]])
--- vim.cmd([[autocmd FileType z80 setlocal ts=4 sts=4 sw=4 noexpandtab]])
-
-vim.opt.title = false
-vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
--- vim.opt.mouse = ""
--- vim.opt.ttymouse = ""
--- vim.opt.mousescroll = "ver:0,hor:0"
-vim.opt.encoding = "utf-8"
-vim.opt.wrap = false
-vim.opt.splitbelow = true
 vim.opt.splitright = true
--- vim.opt.splitkeep = "cursor"
--- vim.opt.mouse = ""
--- vim.opt.formatoptions:remove "o"
--- vim.opt.formatoptions:append({"r"})
-vim.opt.path:append({ "**" })
-vim.opt.wildignore:append({
-	".git",
-	".hg",
-	".svn",
-	"*.o",
-	"*.obj",
-	"*.fbx",
-	"*.FBX",
-	"*.ttf",
-	"*.mp3",
-	"*.wav",
-	"*.ogg",
-	"*.zip",
-	"*.exe",
-	"*.dll",
-	"*.a",
-	"*.so",
-	"*.pyc",
-	"*.class",
-	".idea",
-	"*.swp",
-	"*.DS_Store",
-	"*.meta",
-	"*.ico",
-	"*.tif",
-	"*.bmp",
-	"*.jpg",
-	"*.jpeg",
-	"*.png",
-	"*.tga",
-	"*.psd",
-	"*.svg",
-	"*.bak",
-	"*.cache",
-	"*/out/*",
-	"*/build/*",
-	"*/dist/",
-	"*/node_modules/*",
-})
+vim.opt.splitbelow = true
 
-vim.opt.updatetime = 300
-
-vim.opt.timeout = true
-vim.opt.timeoutlen = 300
+-- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir-nvim"
+vim.opt.undodir = vim.fn.stdpath("config") .. "/undodir-nvim"
+vim.opt.undofile = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.numberwidth = 2
-vim.opt.ruler = false
 vim.opt.signcolumn = "yes"
+vim.opt.numberwidth = 2
 
-vim.opt.cursorline = true
-vim.opt.scrolloff = 5
-vim.opt.backspace = { "indent", "eol", "start" }
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -89,20 +28,46 @@ vim.opt.expandtab = false
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-
-vim.opt.showcmd = true
-vim.opt.showmode = true
-vim.opt.hidden = true
+vim.opt.wrap = false
+vim.opt.showmatch = false
+vim.opt.ruler = false
+vim.opt.title = false
+vim.opt.cursorline = false
 vim.opt.backup = false
 vim.opt.writebackup = false
--- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir-nvim"
-vim.opt.undodir = vim.fn.stdpath("config") .. "/undodir-nvim"
-vim.opt.undofile = true
-vim.opt.history = 20
+
+-- vim.opt.mouse = ""
+-- vim.opt.ttymouse = ""
+-- vim.opt.mousescroll = "ver:0,hor:0"
+vim.opt.encoding = "utf-8"
+vim.opt.backspace = "indent,eol,start"
+vim.opt.clipboard = "unnamed,unnamedplus"
+
+vim.opt.scrolloff = 5
+vim.opt.showcmd = true
+vim.opt.showmode = true
 vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
-vim.opt.inccommand = "split"
+vim.opt.updatetime = 300
+vim.opt.timeout = true
+vim.opt.timeoutlen = 300
+vim.opt.history = 20
+
+vim.opt.hidden = true
+-- vim.opt.inccommand = "split"
+
+vim.opt.wildmenu = true
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wildignore = "*.swp,*.DS_Store,*.meta"
+
+vim.opt.path:append("**")
+
+vim.opt.background = "dark"
+vim.opt.syntax = on
+vim.opt.termguicolors = true
+
+-- NOTE: Will be using night-owl (see plugins.lazy.lua)
+-- vim.cmd([[ colorscheme darcula ]])
+
+vim.cmd([[autocmd BufNewFile,BufRead *.z80.asm set filetype=z80]])
+-- vim.cmd([[autocmd FileType z80 setlocal ts=4 sts=4 sw=4 noexpandtab]])
